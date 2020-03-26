21e18c7dbb0e23d4655aff0ecfb1c21b4c6d3ae8
IbrahimG88 <goharyibrahim@gmail.com>
2020-03-26 13:41:03 +0200

    form completed

--- src/components/ChartOfAccounts.vue
+++ src/components/ChartOfAccounts.vue
@@ -1,47 +1,56 @@
 <template>
   <div>
-     
-      <!-- Add form and its inputs-->
-    <input v-model="Account.name" placeholder="name">
-    <p>name is:{{ Account.name }}</p>
-    <input v-model="Account.type" placeholder="type">
-    <p>Type is: {{ Account.type }}</p>
-    <textarea v-model="Account.description" placeholder="description"></textarea>
-    <p>Description is: {{ Account.description }}</p>
-    <button @click="consoleData">submit</button>
+    <form id="add-account" @submit="consoleData">
+      <input v-model="Account.name" placeholder="name" />
+      <p>name is:{{ Account.name }}</p>
+      <input v-model="Account.type" placeholder="type" />
+      <p>Type is: {{ Account.type }}</p>
+      <textarea v-model="Account.description" placeholder="description"></textarea>
+      <p>Description is: {{ Account.description }}</p>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'ChartOfAccounts',
-    data: () => ({
-    // add all javascript data
-    Account:{
-    name: '',
-    type: '',
-    description: ''
-    },
-    Accounts:[],
-    AccountTypes:[]
-    }),
-    methods:{
-        consoleData () {
-            console.log(this.Account);
-            //this.Account.name = '';
-            //this.Account.type = '';
-            //this.Account.description = '';
-            this.Accounts.push(this.Account);
-
-            if(this.AccountTypes.indexOf(this.Account.type) === -1) {
-                this.AccountTypes.push(this.Account.type);
-                console.log(this.AccountTypes);
-    }
-        }
-    }
+  name: "ChartOfAccounts",
+  data() {
+    return {
+      Account: {
+        name: "",
+        type: "",
+        description: ""
+      },
+      Accounts: [],
+      AccountTypes: []
     };
+  },
+  methods: {
+    consoleData(e) {
+      e.preventDefault();
+      console.log(this.Account);
+
+      let Account = {
+        name: this.Account.name,
+        type: this.Account.type,
+        description: this.Account.description
+      };
+
+      this.Accounts.push(Account);
+
+      if (this.AccountTypes.indexOf(this.Account.type) === -1) {
+        this.AccountTypes.push(this.Account.type);
+        console.log(this.AccountTypes);
+      }
+
+      this.Account.name = "";
+      this.Account.type = "";
+      this.Account.description = "";
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
