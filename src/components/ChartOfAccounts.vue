dee5bf1bf55dbb5b8646bd6438a8b91c27315315
IbrahimG88 <goharyibrahim@gmail.com>
2020-03-26 15:59:01 +0200

    chart almost completed to add a delete vutton

--- src/components/ChartOfAccounts.vue
+++ src/components/ChartOfAccounts.vue
@@ -1,23 +1,38 @@
 <template>
   <div>
-    <form id="add-account" @submit="consoleData">
-      <input v-model="Account.name" placeholder="name" />
-      <p>name is:{{ Account.name }}</p>
-      <input v-model="Account.type" placeholder="type" />
-      <p>Type is: {{ Account.type }}</p>
-      <textarea v-model="Account.description" placeholder="description"></textarea>
-      <p>Description is: {{ Account.description }}</p>
-      <input type="submit" value="Submit" />
-    </form>
-    <div>
-      <ul>
-        <button @click="toggleShow">toggleshow</button>
-        <li v-bind:key="account.name" v-for="account in Accounts">
-          {{ account.type }}
-          <div>{{ show == 1 ? account.description: null }}</div>
-        </li>
-      </ul>
-    </div>
+    <v-form ref="form" class="form">
+      <v-text-field
+        v-model="Account.name"
+        :counter="20"
+        label="Name"
+        required
+      ></v-text-field>
+
+
+      <v-text-field
+        v-model="Account.type"
+        label="type"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="Account.description"
+        label="description"
+        required
+      ></v-text-field>
+
+
+     
+
+      <v-btn
+        color="success"
+        class="mr-4"
+        @click="consoleData"
+      >
+        Add account
+      </v-btn>
+    </v-form>
+ 
     <v-simple-table fixed-header height="300px">
       <template v-slot:default>
         <thead>
@@ -77,14 +92,14 @@ export default {
       this.Account.type = "";
       this.Account.description = "";
 
-    },
-    toggleShow() {
-      this.show = !this.show;
-    },
-    
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.form {
+  width: 400px;
+  margin-left: 30px;
+}
 </style>
